f30ac3d643a25ac83d992f66c34c289de352876a
Danny Flores <djoeflores@gmail.com>
2017-10-31 21:52:36 -0500

    First Commit!

---
+++ UT_for_CoreConsole/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UT_for_CoreConsole {
+    [TestClass]
+    public class UnitTestSoldier {
+        [TestMethod]
+        public void TestConstructorA() {
+            
+        }
+
+        [TestMethod]
+        public void TestConstructorB() {
+
+        }
+
+        [TestMethod]
+        public void TestConstructorC() {
+
+        }
+    }
+}
